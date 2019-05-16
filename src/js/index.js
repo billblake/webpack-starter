abd2d94594f52dbe40769e3fbceb2593cc54de77
Bill Blake <billblake01@yahoo.ie>
2019-05-16 21:02:14 +0100

    Added eslint

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,28 @@
-import _ from 'lodash';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../css/style.css'
+import '../sass/style.scss'
+// eslint-disable-next-line id-length
 import $ from 'jquery'
-import '../css/style.css';
-import '../sass/style.scss';
-import 'bootstrap';
-import "bootstrap/dist/css/bootstrap.min.css";
+// eslint-disable-next-line id-length
+import _ from 'lodash'
 
-$(document).ready(function () {
-    
-    // lodash example usage
-    const lodashExample = _.join(['Hello', 'World'], ' ');
+// Load handlebars template
+const template = require('../templates/userTable.handlebars')
 
-    // load handlebars template
-    const template = require("../templates/userTable.handlebars");
-    
-    // jquery
-    $("#save").click(function () {
+$(document).ready(() => {
+    // Lodash example usage
+    // eslint-disable-next-line no-unused-vars,array-element-newline
+    const lodashExample = _.join(['Hello', 'World'], ' ')
+
+    // Jquery
+    $('#save').click(() => {
         // Bootstrap js
         $('#exampleModal').modal('hide')
-    });
-    
-    // example get service call
-    $.get("/user").then((response) => {
-        $("#user-table-wrapper").html(template({users : JSON.parse(response)}))
-    });
+    })
+
+    // Example get service call
+    $.get('/user').then((response) => {
+        $('#user-table-wrapper').html(template({ 'users': JSON.parse(response) }))
+    })
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,78 @@
+/* eslint-disable require-unicode-regexp */
+/* eslint-disable strict */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/js/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        port: 8000,
-        contentBase: './dist',
-        proxy: {
+    'devServer': {
+        'contentBase': './dist',
+        'port': 8000,
+        'proxy': {
             '/': {
-              target: 'http://localhost:8080',
+                'target': 'http://localhost:8080'
             }
-          }
+        }
     },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [  // Array of plugins to apply to build chunk
-        new HtmlWebpackPlugin({
-            template: __dirname + "/src/public/index.html",
-            inject: 'body'
-        })
-    ],
-    module: {
-        rules: [
+    'devtool': 'inline-source-map',
+    'entry': './src/js/index.js',
+    'mode': 'development',
+    'module': {
+        'rules': [
             {
-                test: /\.css$/,
-                use: [
+                'test': /\.css$/,
+                'use': [
                     'style-loader',
                     'css-loader'
                 ]
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            { 
-                test: /\.handlebars$/,
-                loader: "handlebars-loader"
+                // eslint-disable-next-line prefer-named-capture-group
+                'test': /\.(png|svg|jpg|gif)$/,
+                'use': ['file-loader']
             },
             {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
+                'loader': 'handlebars-loader',
+                'test': /\.handlebars$/
+            },
+            {
+                // eslint-disable-next-line prefer-named-capture-group
+                'exclude': /(node_modules)/,
+                'test': /\.js$/,
+                'use': {
+                    'loader': 'babel-loader',
+                    'options': {
+                        'presets': [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
                     }
                 }
             },
             {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
+                'test': /\.scss$/,
+                'use': [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
                 ]
+            },
+            {
+                'enforce': 'pre',
+                'exclude': /node_modules/,
+                'loader': 'eslint-loader',
+                'test': /\.js$/
             }
         ]
-    }
+    },
+    'output': {
+        'filename': 'bundle.js',
+        'path': path.resolve(__dirname, 'dist')
+    },
+    'plugins': [
+        // Array of plugins to apply to build chunk
+        new HtmlWebpackPlugin({
+            'inject': 'body',
+            'template': `${__dirname}/src/public/index.html`
+        })
+    ]
 };
